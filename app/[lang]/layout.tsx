109955504257bba30189e1432d83490e2ae4e456
zealleaf <github@zealleaf.me>
2023-10-19 15:42:13 +0800

    chore: some change

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,52 +1,30 @@
-import {ThemeProvider, ThemeToggler} from '@/components/Feature/DarkMode';
+import {ThemeToggler} from '@/components/Feature/DarkMode';
 import {LangToggler} from '@/components/Feature/I18n';
 import {Footer} from '@/components/Layout/Footer';
 import {TopNav} from '@/components/Layout/TopNav';
 import '@/public/globals.css';
-import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
 import {siteConfig} from '../siteConfig';
 import {topNavLinks} from '../topNavLinks';
 
-const inter = Inter({subsets: ['latin']});
-
-export const runtime = 'edge';
-
-export const metadata: Metadata = {
-  title: 'Image Converter',
-  description: 'A simple image conversion tool online website',
-  icons: '/favicon.ico',
-};
-
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function LangLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <div>
-            <TopNav
-              siteName={siteConfig.siteName}
-              links={topNavLinks}
-              extra={
-                <div className="flex gap-2">
-                  <LangToggler />
-                  <ThemeToggler />
-                </div>
-              }
-            />
-            <div className="min-h-[calc(100vh-65px-64px)]">
-              <div className="mx-6 h-full py-6 sm:container sm:mx-auto">
-                {children}
-              </div>
-            </div>
-            <Footer copyright={siteConfig.copyright} />
+    <div>
+      <TopNav
+        siteName={siteConfig.siteName}
+        links={topNavLinks}
+        extra={
+          <div className="flex gap-2">
+            <LangToggler />
+            <ThemeToggler />
           </div>
-        </ThemeProvider>
-      </body>
-    </html>
+        }
+      />
+      <div className="min-h-[calc(100vh-65px-64px)]">
+        <div className="mx-6 h-full py-6 sm:container sm:mx-auto">
+          {children}
+        </div>
+      </div>
+      <Footer copyright={siteConfig.copyright} />
+    </div>
   );
 }
--- app/error.tsx
+++ app/error.tsx
@@ -2,13 +2,13 @@
 
 export default function Error() {
   return (
-    <div className="grid h-full place-items-center text-2xl">
+    <div className="grid h-screen place-items-center text-2xl">
       <div className="flex">
         <div className="animate-bounce">e</div>
         <div className="animate-bounce delay-100">r</div>
         <div className="animate-bounce delay-200">r</div>
         <div className="animate-bounce delay-300">o</div>
-        <div className="delay-400 animate-bounce">r</div>
+        <div className="animate-bounce delay-400">r</div>
       </div>
     </div>
   );
